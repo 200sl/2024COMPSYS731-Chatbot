ae898c0c3f9b2efd85152df9c16945c8a4ce42fb
mmmanticore <76192603+mmmanticore@users.noreply.github.com>
2024-10-09 13:32:49 +1300

    Add files via upload

--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 import torch
 import torch.nn as nn
-import torchvision.datasets as datasets
-from torchvision.models import AlexNet_Weights
+import torchvision.datasets as datasets 
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision import models
+from dataset import CustomImageDataset
 from earlystopping import EarlyStopping
-import matplotlib.pyplot as plt
-import time
+from sklearn.metrics import precision_score, recall_score, fbeta_score
 
 
 class Trainer:
@@ -20,16 +19,17 @@ class Trainer:
         ])
                     
         # Load data set
-        self.train_dataset = datasets.ImageFolder(root='./dataset/train_images', transform=transform)
-        self.test_dataset = datasets.ImageFolder(root='./dataset/test_images', transform=transform)
+        self.train_dataset = datasets.ImageFolder(root='../AlexNet/train_images', transform=transform)
+        self.test_dataset = datasets.ImageFolder(root='../AlexNet/test_images', transform=transform)
+        print(self.train_dataset.classes)
 
         # Create data loaders
-        self.train_loader = DataLoader(self.train_dataset, batch_size=32, shuffle=True)
-        self.test_loader = DataLoader(self.test_dataset, batch_size=32, shuffle=False)
+        self.train_loader = DataLoader(self.train_dataset, batch_size=10, shuffle=True)
+        self.test_loader = DataLoader(self.test_dataset, batch_size=10, shuffle=False)
 
         # Load the pre-trained AlexNet model
         self.model = models.alexnet(weights=None)
-        # self.model.load_state_dict(torch.load('alexnet-owt-7be5be79.pth', weights_only=True))
+        self.model.load_state_dict(torch.load('alexnet-owt-7be5be79.pth', weights_only=True))
 
         # Modify the last layer for the number of categories
         num_classes = 7  # Adjusted for the number of categories in the dataset
@@ -49,16 +49,11 @@ class Trainer:
     def train(self, num_epochs, early_stopping):
         best_accuracy = 0.0  # Initialize best accuracy
 
-        acc_list = []
-        loss_list = []
-
         for epoch in range(num_epochs):
             self.model.train()
             running_loss = 0.0
 
             # Training loop
-            progess_count = 0
-            start_time = time.time()
             for images, labels in self.train_loader:
                 images, labels = images.to(self.device), labels.to(self.device)
 
@@ -67,13 +62,9 @@ class Trainer:
                 loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
-
                 running_loss += loss.item()
-                progess_count += 1
-                if progess_count % 50 == 0:
-                    print(f'Step [{progess_count}/{len(self.train_loader)}]')
 
-            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(self.train_loader):.4f}, time cost: {time.time()-start_time:.2f}s')
+            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(self.train_loader):.4f}')
 
             # Print the current learning rate
             current_lr = self.scheduler.get_last_lr()[0]
@@ -84,13 +75,9 @@ class Trainer:
 
             # Evaluate the model on the test set
             accuracy = self.evaluate()
-            avg_loss = running_loss / len(self.train_loader)
-
-            acc_list.append(accuracy)
-            loss_list.append(avg_loss)
 
             # Save the model if the accuracy is the best we've seen so far
-            if accuracy > best_accuracy and accuracy > 75.0:
+            if accuracy > best_accuracy:
                 best_accuracy = accuracy
                 torch.save(self.model.state_dict(), 'alexnet_face_recognition.pt')
                 print(f"Saved Best Model with Accuracy: {best_accuracy}%")
@@ -102,23 +89,37 @@ class Trainer:
                 print(f"Early stopping at epoch {epoch+1}")
                 break
 
-        return acc_list, loss_list
-
     def evaluate(self):
         self.model.eval()
         correct = 0
         total = 0
+
+        all_labels = []
+        all_predictions = []
 
         with torch.no_grad():
             for images, labels in self.test_loader:
                 images, labels = images.to(self.device), labels.to(self.device)
                 outputs = self.model(images)
                 _, predicted = torch.max(outputs.data, 1)
+
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
+                # Save all labels and predictions
+                all_labels.extend(labels.cpu().numpy())
+                all_predictions.extend(predicted.cpu().numpy())
+
         accuracy = 100 * correct / total
         print(f'Validation Accuracy: {accuracy}%')
+
+        # Calculate Precision, Recall, and F2-Score
+        precision = precision_score(all_labels, all_predictions, average='weighted')
+        recall = recall_score(all_labels, all_predictions, average='weighted')
+        f2 = fbeta_score(all_labels, all_predictions, beta=2, average='weighted')
+
+        print(f'Precision: {precision:.4f}, Recall: {recall:.4f}, F2-Score: {f2:.4f}')
+
         return accuracy
 
 
@@ -126,22 +127,7 @@ class Trainer:
 trainer = Trainer()
 
 # Create an EarlyStopping instance with patience=10
-early_stopping = EarlyStopping(patience=10, delta=0)
+early_stopping = EarlyStopping(patience=20, delta=0)
 
 # Start training with early stopping
-acc_list, loss_list = trainer.train(64, early_stopping)
-
-plt.subplot(1, 2, 1)
-plt.plot(acc_list, label='Accuracy')
-plt.title("Epoch vs Accuracy")
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(loss_list, label='Loss')
-plt.title("Epoch vs Loss")
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-
-plt.show()
-
+trainer.train(200, early_stopping)
